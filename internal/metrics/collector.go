8b1ee07fb90bc616c408fb344d1034f7621cb03e
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-04-22 20:12:27 +0200

    fix metrics

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -7,15 +7,27 @@ import (
 	"skylytics/internal/core"
 	"skylytics/pkg/async"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/samber/do"
 )
 
+var (
+	tableCount = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "skylytics_table_estimated_count",
+		Help: "Estimated record count for a table.",
+	}, []string{"table"})
+)
+
 type Collector struct {
+	db     core.DB
 	handle *async.JobHandle[any]
 }
 
-func NewCollector(_ *do.Injector) (core.MetricsCollector, error) {
-	collector := Collector{}
+func NewCollector(i *do.Injector) (core.MetricsCollector, error) {
+	collector := Collector{
+		db: do.MustInvoke[core.DB](i),
+	}
 
 	collector.handle = async.Job(func(ctx context.Context) (any, error) {
 		ticker := time.NewTicker(15 * time.Second)
@@ -27,6 +39,19 @@ func NewCollector(_ *do.Injector) (core.MetricsCollector, error) {
 
 				return nil, nil
 			case <-ticker.C:
+				var count int64
+				err := collector.db.Model(&core.EventModel{}).
+					Raw(
+						`SELECT reltuples::bigint AS count 
+						FROM pg_class 
+						WHERE relname = ?`, core.EventModel{}.TableName(),
+					).
+					Scan(&count).Error
+
+				if err != nil {
+					return nil, err
+				}
+				tableCount.WithLabelValues("events").Set(float64(count))
 			}
 		}
 	})
