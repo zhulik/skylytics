84c92063acdf7ce367d2e22ee913a8db310fbc87
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-04-22 14:46:11 +0200

    Remove mongodb from collector

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -2,25 +2,12 @@ package metrics
 
 import (
 	"context"
-	"os"
 	"time"
 
 	"skylytics/internal/core"
-	"skylytics/internal/persistence"
 	"skylytics/pkg/async"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/samber/do"
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
-)
-
-var (
-	collectionCount = promauto.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "mongodb_collection_estimated_count",
-		Help: "Estimated document count in MongoDB collection",
-	}, []string{"collection"})
 )
 
 type Collector struct {
@@ -33,23 +20,6 @@ func NewCollector(_ *do.Injector) (core.MetricsCollector, error) {
 	collector.handle = async.Job(func(ctx context.Context) (any, error) {
 		ticker := time.NewTicker(15 * time.Second)
 
-		uri := os.Getenv("MONGODB_URI")
-		if uri == "" {
-			return nil, persistence.ErrNoMongodbURI
-		}
-
-		client, err := mongo.Connect(options.Client().ApplyURI(uri))
-		if err != nil {
-			return nil, err
-		}
-
-		defer client.Disconnect(ctx) //nolint:errcheck
-
-		db := client.Database("admin")
-
-		accounts := db.Collection("accounts")
-		events := db.Collection("events")
-
 		for {
 			select {
 			case <-ctx.Done():
@@ -57,17 +27,6 @@ func NewCollector(_ *do.Injector) (core.MetricsCollector, error) {
 
 				return nil, nil
 			case <-ticker.C:
-				count, err := accounts.EstimatedDocumentCount(ctx, nil)
-				if err != nil {
-					return nil, err
-				}
-				collectionCount.WithLabelValues("accounts").Set(float64(count))
-
-				count, err = events.EstimatedDocumentCount(ctx, nil)
-				if err != nil {
-					return nil, err
-				}
-				collectionCount.WithLabelValues("events").Set(float64(count))
 			}
 		}
 	})
