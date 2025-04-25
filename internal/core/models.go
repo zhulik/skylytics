17b7a7ed2ee1a7b0d5b5aacd92cc84a54f045da2
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-04-25 22:28:13 +0200

    Update data models

--- internal/core/models.go
+++ internal/core/models.go
@@ -1,12 +1,16 @@
 package core
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 // EventModel represents a raw bluesky jetstream event.
 type EventModel struct {
-	gorm.Model
+	CreatedAt time.Time `gorm:"nullable:false"`
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	Event BlueskyEvent `gorm:"type:jsonb;index:idx_event_did,expression:((event->>'did'));index:idx_event_kind,expression:((event->>'kind'));index:idx_event_time_us,expression:((event->>'time_us'))"`
 }
@@ -17,7 +21,9 @@ func (EventModel) TableName() string {
 
 // AccountModel represents a raw bluesky account event.
 type AccountModel struct {
-	gorm.Model
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	Account []byte `gorm:"type:jsonb;index:idx_did,expression:((account->>'did')),unique"`
 }
