994c1b6c9a43318a0b928e4953179fe8b04fd355
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-11 01:08:34 +0200

    Only ack in account-updater

--- internal/updating/pipeline.go
+++ internal/updating/pipeline.go
@@ -3,13 +3,11 @@ package updating
 import (
 	"context"
 	"encoding/json"
-	"errors"
 
 	"skylytics/internal/core"
 	"skylytics/pkg/async"
 	"skylytics/pkg/stormy"
 
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/samber/lo"
 
@@ -29,32 +27,32 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 	return pips.New[jetstream.Msg, any]().
 		Then(parseDIDs).
 		Then(apply.Batch[pips.P[jetstream.Msg, string]](1000)).
-		Then(filterOutExistingAccounts(updater.AccountRepo)).
-		Then(apply.Rebatch[pips.P[jetstream.Msg, string]](25)).
+		// Then(filterOutExistingAccounts(updater.AccountRepo)).
+		// Then(apply.Rebatch[pips.P[jetstream.Msg, string]](25)).
 		Then(
 			apply.Map(func(ctx context.Context, wraps []pips.P[jetstream.Msg, string]) (any, error) {
-				dids := lo.Map(wraps, func(item pips.P[jetstream.Msg, string], _ int) string {
-					return item.B()
-				})
-
-				serializedProfiles, err := fetchAndSerializeProfiles(ctx, updater.Stormy, dids)
-				if err != nil {
-					return nil, err
-				}
-
-				err = updater.AccountRepo.Insert(ctx, serializedProfiles...)
-				if err != nil {
-					var pgError *pgconn.PgError
-					if errors.As(err, &pgError) {
-						// Ignore duplicate key errors.
-						if pgError.Code != "23505" &&
-							pgError.Code != "40P01" {
-							return nil, err
-						}
-					} else {
-						return nil, err
-					}
-				}
+				// dids := lo.Map(wraps, func(item pips.P[jetstream.Msg, string], _ int) string {
+				// 	return item.B()
+				// })
+				//
+				// serializedProfiles, err := fetchAndSerializeProfiles(ctx, updater.Stormy, dids)
+				// if err != nil {
+				// 	return nil, err
+				// }
+				//
+				// err = updater.AccountRepo.Insert(ctx, serializedProfiles...)
+				// if err != nil {
+				// 	var pgError *pgconn.PgError
+				// 	if errors.As(err, &pgError) {
+				// 		// Ignore duplicate key errors.
+				// 		if pgError.Code != "23505" &&
+				// 			pgError.Code != "40P01" {
+				// 			return nil, err
+				// 		}
+				// 	} else {
+				// 		return nil, err
+				// 	}
+				// }
 				lo.ForEach(wraps, func(item pips.P[jetstream.Msg, string], _ int) {
 					if item.A() != nil {
 						item.A().Ack() //nolint:errcheck
