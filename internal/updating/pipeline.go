b26af05ba55e591c38d6662c0dcd4a26de2cb4d0
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-11 01:11:00 +0200

    linter

--- internal/updating/pipeline.go
+++ internal/updating/pipeline.go
@@ -5,8 +5,6 @@ import (
 	"encoding/json"
 
 	"skylytics/internal/core"
-	"skylytics/pkg/async"
-	"skylytics/pkg/stormy"
 
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/samber/lo"
@@ -23,14 +21,14 @@ var (
 	})
 )
 
-func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
+func pipeline(_ *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 	return pips.New[jetstream.Msg, any]().
 		Then(parseDIDs).
 		Then(apply.Batch[pips.P[jetstream.Msg, string]](1000)).
 		// Then(filterOutExistingAccounts(updater.AccountRepo)).
 		// Then(apply.Rebatch[pips.P[jetstream.Msg, string]](25)).
 		Then(
-			apply.Map(func(ctx context.Context, wraps []pips.P[jetstream.Msg, string]) (any, error) {
+			apply.Map(func(_ context.Context, wraps []pips.P[jetstream.Msg, string]) (any, error) {
 				// dids := lo.Map(wraps, func(item pips.P[jetstream.Msg, string], _ int) string {
 				// 	return item.B()
 				// })
@@ -64,44 +62,45 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 		)
 }
 
-func fetchAndSerializeProfiles(ctx context.Context, strmy *stormy.Client, dids []string) ([]core.AccountModel, error) {
-	profiles, err := strmy.GetProfiles(ctx, dids...)
-	if err != nil {
-		return nil, err
-	}
-
-	return async.AsyncMap(ctx, profiles, func(_ context.Context, profile *stormy.Profile) (core.AccountModel, error) {
-		account, err := json.Marshal(profile)
-		if err != nil {
-			return core.AccountModel{}, err
-		}
-		var accountModel core.AccountModel
-
-		err = json.Unmarshal(account, &accountModel)
-		if err != nil {
-			return core.AccountModel{}, err
-		}
-
-		return core.AccountModel{Account: account}, nil
-	})
-}
-
-func filterOutExistingAccounts(repo core.AccountRepository) pips.Stage {
-	return apply.Map(func(ctx context.Context, wraps []pips.P[jetstream.Msg, string]) ([]pips.P[jetstream.Msg, string], error) {
-		dids := lo.Map(wraps, func(item pips.P[jetstream.Msg, string], _ int) string {
-			return item.B()
-		})
-
-		existing, err := repo.ExistsByDID(ctx, dids...)
-		if err != nil {
-			return nil, err
-		}
-		return lo.Reject(wraps, func(item pips.P[jetstream.Msg, string], _ int) bool {
-			if lo.Contains(existing, item.B()) {
-				item.A().Ack() //nolint:errcheck
-				return true
-			}
-			return false
-		}), nil
-	})
-}
+//
+// func fetchAndSerializeProfiles(ctx context.Context, strmy *stormy.Client, dids []string) ([]core.AccountModel, error) {
+// 	profiles, err := strmy.GetProfiles(ctx, dids...)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+//
+// 	return async.AsyncMap(ctx, profiles, func(_ context.Context, profile *stormy.Profile) (core.AccountModel, error) {
+// 		account, err := json.Marshal(profile)
+// 		if err != nil {
+// 			return core.AccountModel{}, err
+// 		}
+// 		var accountModel core.AccountModel
+//
+// 		err = json.Unmarshal(account, &accountModel)
+// 		if err != nil {
+// 			return core.AccountModel{}, err
+// 		}
+//
+// 		return core.AccountModel{Account: account}, nil
+// 	})
+// }
+//
+// func filterOutExistingAccounts(repo core.AccountRepository) pips.Stage {
+// 	return apply.Map(func(ctx context.Context, wraps []pips.P[jetstream.Msg, string]) ([]pips.P[jetstream.Msg, string], error) {
+// 		dids := lo.Map(wraps, func(item pips.P[jetstream.Msg, string], _ int) string {
+// 			return item.B()
+// 		})
+//
+// 		existing, err := repo.ExistsByDID(ctx, dids...)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		return lo.Reject(wraps, func(item pips.P[jetstream.Msg, string], _ int) bool {
+// 			if lo.Contains(existing, item.B()) {
+// 				item.A().Ack() //nolint:errcheck
+// 				return true
+// 			}
+// 			return false
+// 		}), nil
+// 	})
+// }
