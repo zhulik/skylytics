e4a6dcea74563d1a41777585511b2c18c3b501a6
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-10 19:02:06 +0200

    Fix batching

--- internal/nats/client.go
+++ internal/nats/client.go
@@ -69,7 +69,7 @@ func (c *Client) Consume(ctx context.Context, stream, name string) (<-chan pips.
 				return nil
 
 			default:
-				batch, err := cons.FetchNoWait(1000)
+				batch, err := cons.Fetch(100)
 				if err != nil {
 					y(nil, err)
 				}
