3338d996e8d6597cce09408643ef7b6a73405523
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-11 01:45:43 +0200

    Fix

--- internal/nats/client.go
+++ internal/nats/client.go
@@ -69,7 +69,7 @@ func (c *Client) Consume(ctx context.Context, stream, name string) (<-chan pips.
 				return nil
 
 			default:
-				batch, err := cons.FetchNoWait(100)
+				batch, err := cons.FetchNoWait(10000)
 				if err != nil {
 					y(nil, err)
 				}
