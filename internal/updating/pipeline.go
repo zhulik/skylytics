cd47a1d47ddd91c5065c41893b39aa6b87d4e872
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-11 13:30:08 +0200

    Ack all msgs in the updater pipeline in the very beginning

--- internal/updating/pipeline.go
+++ internal/updating/pipeline.go
@@ -37,6 +37,9 @@ var (
 
 func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 	return pips.New[jetstream.Msg, any]().
+		Then(apply.Each(func(_ context.Context, msg jetstream.Msg) error {
+			return msg.Ack()
+		})).
 		Then(parseDIDs).
 		Then(apply.Batch[pips.P[jetstream.Msg, string]](1000)).
 		Then(filterOutExistingAccounts(updater.AccountRepo)).
