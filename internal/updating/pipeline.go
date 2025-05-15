b623059019e2128b64737f2935bcaf49511ea98e
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-15 23:52:12 +0200

    Use EachC and MapC

--- internal/updating/pipeline.go
+++ internal/updating/pipeline.go
@@ -17,8 +17,6 @@ import (
 	"github.com/nats-io/nats.go/jetstream"
 	"github.com/samber/lo"
 
-	"golang.org/x/sync/errgroup"
-
 	"github.com/zhulik/pips"
 	"github.com/zhulik/pips/apply"
 )
@@ -45,7 +43,7 @@ type pipelineItem struct {
 func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 	return pips.New[jetstream.Msg, any]().
 		Then( // Parse items
-			apply.Map(func(_ context.Context, msg jetstream.Msg) (pipelineItem, error) {
+			apply.MapC(4, func(_ context.Context, msg jetstream.Msg) (pipelineItem, error) {
 				event := &core.BlueskyEvent{}
 				err := json.Unmarshal(msg.Data(), event)
 				if err != nil {
@@ -60,14 +58,14 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 			}),
 		).
 		Then(
-			apply.Each(func(_ context.Context, item pipelineItem) error {
+			apply.EachC(4, func(_ context.Context, item pipelineItem) error {
 				item.msg.InProgress() // nolint:errcheck
 				return nil
 			}),
 		).
 		Then(apply.Batch[pipelineItem](1000)).
 		Then( // Fetch and set existing records
-			apply.Map(func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
+			apply.MapC(4, func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
 				updater.Logger.Info("Processing batch", "batch_size", len(items))
 
 				dids := lo.Map(items, func(item pipelineItem, _ int) string {
@@ -101,9 +99,9 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 				return true, nil
 			}),
 		).
-		Then(apply.Batch[pipelineItem](100)).
+		Then(apply.Batch[pipelineItem](25)).
 		Then( // Fetch profiles
-			apply.Map(func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
+			apply.MapC(4, func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
 				dids := lo.Map(items, func(item pipelineItem, _ int) string {
 					return item.event.Did
 				})
@@ -124,7 +122,7 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 			}),
 		).
 		Then( // Try to insert in batches
-			apply.Map(func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
+			apply.MapC(4, func(ctx context.Context, items []pipelineItem) ([]pipelineItem, error) {
 				serializedProfiles := lo.Map(items, func(item pipelineItem, _ int) core.AccountModel {
 					return item.account
 				})
@@ -161,53 +159,30 @@ func pipeline(updater *AccountUpdater) *pips.Pipeline[jetstream.Msg, any] {
 }
 
 func fetchAndSerializeProfiles(ctx context.Context, strmy *stormy.Client, dids []string) (map[string]core.AccountModel, error) {
-	g, ctx := errgroup.WithContext(ctx)
-	resultChan := make(chan []core.AccountModel, (len(dids)+24)/25)
-
-	chunks := lo.Chunk(dids, 25)
-	for _, chunk := range chunks {
-		didChunk := chunk
-		g.Go(func() error {
-			profiles, err := strmy.GetProfiles(ctx, didChunk...)
-			if err != nil {
-				return err
-			}
-
-			models, err := async.AsyncMap(ctx, profiles, func(_ context.Context, profile *stormy.Profile) (core.AccountModel, error) {
-				account, err := json.Marshal(profile)
-				if err != nil {
-					return core.AccountModel{}, err
-				}
-				var accountModel core.AccountModel
-
-				err = json.Unmarshal(account, &accountModel)
-				if err != nil {
-					return core.AccountModel{}, err
-				}
-
-				return core.AccountModel{Account: account, DID: profile.DID}, nil
-			})
-			if err != nil {
-				return err
-			}
-
-			resultChan <- models
-			return nil
-		})
-	}
-
-	if err := g.Wait(); err != nil {
+	profiles, err := strmy.GetProfiles(ctx, dids...)
+	if err != nil {
 		return nil, err
 	}
 
-	close(resultChan)
+	models, err := async.AsyncMap(ctx, profiles, func(_ context.Context, profile *stormy.Profile) (core.AccountModel, error) {
+		account, err := json.Marshal(profile)
+		if err != nil {
+			return core.AccountModel{}, err
+		}
+		var accountModel core.AccountModel
 
-	var result []core.AccountModel
-	for models := range resultChan {
-		result = append(result, models...)
+		err = json.Unmarshal(account, &accountModel)
+		if err != nil {
+			return core.AccountModel{}, err
+		}
+
+		return core.AccountModel{Account: account, DID: profile.DID}, nil
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	return lo.Associate(result, func(item core.AccountModel) (string, core.AccountModel) {
+	return lo.Associate(models, func(item core.AccountModel) (string, core.AccountModel) {
 		return item.DID, item
 	}), nil
 }
