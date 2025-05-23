4cc0b53a3087173668315956ae354a0d93ed0beb
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2025-05-23 22:17:12 +0200

    Fix

--- internal/analytics/post_stats_collector.go
+++ internal/analytics/post_stats_collector.go
@@ -56,6 +56,7 @@ func (p *PostStatsCollector) Run(ctx context.Context) error {
 						return err
 					}
 					interactionsProcessed.WithLabelValues(event.Commit.Collection).Inc()
+					return nil
 				}),
 			).
 			Then(
