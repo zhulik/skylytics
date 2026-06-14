d95a6e55dee5e12839fe57920c6d0f06407f706c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2026-06-14 13:50:23 +0200

    Minor refactoring

--- internal/analyzer/leaderboard_bucket_saver.go
+++ internal/analyzer/leaderboard_bucket_saver.go
@@ -16,46 +16,46 @@ type LeaderboardBucketSaver struct {
 }
 
 func (s *LeaderboardBucketSaver) SaveLike(ctx context.Context, like *apibsky.FeedLike) error {
-	createdAt, err := time.Parse(time.RFC3339, like.CreatedAt)
-	if err != nil {
-		return err
-	}
-	return s.ZincrExpire(ctx, leaderboard.FineMinuteKey(core.Like, createdAt), like.Subject.Uri)
+	return s.zincrInteractionCounters(ctx, core.Like, like.CreatedAt, like.Subject.Uri)
 }
 
 func (s *LeaderboardBucketSaver) SaveRepost(ctx context.Context, repost *apibsky.FeedRepost) error {
-	createdAt, err := time.Parse(time.RFC3339, repost.CreatedAt)
-	if err != nil {
-		return err
-	}
-	return s.ZincrExpire(ctx, leaderboard.FineMinuteKey(core.Repost, createdAt), repost.Subject.Uri)
+	return s.zincrInteractionCounters(ctx, core.Repost, repost.CreatedAt, repost.Subject.Uri)
 }
 
 func (s *LeaderboardBucketSaver) SaveQuote(ctx context.Context, createdAt string, embed *apibsky.EmbedRecord) error {
-	t, err := time.Parse(time.RFC3339, createdAt)
-	if err != nil {
-		return err
-	}
-	return s.ZincrExpire(ctx, leaderboard.FineMinuteKey(core.Quote, t), embed.Record.Uri)
+	return s.zincrInteractionCounters(ctx, core.Quote, createdAt, embed.Record.Uri)
 }
 
 func (s *LeaderboardBucketSaver) SaveReply(ctx context.Context, createdAt string, reply *apibsky.FeedPost_ReplyRef) error {
-	t, err := time.Parse(time.RFC3339, createdAt)
+	return s.zincrReplyCounters(ctx, createdAt, reply.Parent.Uri, reply.Root.Uri)
+}
+
+func (s *LeaderboardBucketSaver) zincrInteractionCounters(ctx context.Context, interaction core.Interaction, timestamp, postID string) error {
+	t, err := time.Parse(time.RFC3339, timestamp)
+	if err != nil {
+		return err
+	}
+	return s.zincrExpire(ctx, leaderboard.FineMinuteKey(interaction, t), postID, 48*time.Hour)
+}
+
+func (s *LeaderboardBucketSaver) zincrReplyCounters(ctx context.Context, timestamp, parentID, rootID string) error {
+	t, err := time.Parse(time.RFC3339, timestamp)
 	if err != nil {
 		return err
 	}
 	key := leaderboard.FineMinuteKey(core.Reply, t)
-	err = s.ZincrExpire(ctx, key, reply.Parent.Uri)
+	err = s.zincrExpire(ctx, key, parentID, 48*time.Hour)
 	if err != nil {
 		return err
 	}
-	return s.ZincrExpire(ctx, key, reply.Root.Uri)
+	return s.zincrExpire(ctx, key, rootID, 48*time.Hour)
 }
 
-func (s *LeaderboardBucketSaver) ZincrExpire(ctx context.Context, key, postID string) error {
+func (s *LeaderboardBucketSaver) zincrExpire(ctx context.Context, key, postID string, duration time.Duration) error {
 	err := s.Redis.ZIncrBy(ctx, key, 1, postID).Err()
 	if err != nil {
 		return err
 	}
-	return s.Redis.Expire(ctx, key, 48*time.Hour).Err()
+	return s.Redis.Expire(ctx, key, duration).Err()
 }
